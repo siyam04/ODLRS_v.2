2472c72b0a88c3a3bb7d24e6d913892fd5b00a93
icecream <zanjarwhite@gmail.com>
2019-05-20 00:07:19 +0600

    templates updated

--- source/urls.py
+++ source/urls.py
@@ -1,25 +1,10 @@
-"""source URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 
-from .views import Home
-
+from .views import Home, MyLoginView, MySignupView
 
 urlpatterns = [
 
@@ -36,6 +21,8 @@ urlpatterns = [
     # Built-in Auth PATHs
     path('accounts/', include('allauth.urls')),
 
+    path('login/',MyLoginView.as_view(),name='account_login'),
+    path('signup/',MySignupView.as_view(),name='account_signup'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- source/views.py
+++ source/views.py
@@ -3,10 +3,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-
+from allauth.account.views import SignupView,LoginView
+from .forms import LoginForm,SignupForm
 
 class Home(TemplateView):
     template_name = 'home.html'
+
+
+class MyLoginView(LoginView):
+    form_class=LoginForm
+    template_name='login.html'
+
+class MySignupView(SignupView):
+    form_class=SignupForm
+    template_name='signup.html'
 
 
 
