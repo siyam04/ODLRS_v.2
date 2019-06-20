2f9dc005414bdbb0da08bfca422c2012d4f1bce1
nazmul629 <nazmulhossain.qnh@gmail.com>
2019-06-20 16:22:59 +0600

    search & paginator added, need to update later

--- diagnostic_centers/urls.py
+++ diagnostic_centers/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (
 
-    AllDiagnosticCenters,
+    search_paginator,
 
     admin_login,
     admin_logout,
@@ -18,8 +18,8 @@ app_name = 'diagnostic_centers'
 
 urlpatterns = [
 
-    path('all-centers/', AllDiagnosticCenters.as_view(), name='all-centers'),
-
+    # path('all-centers/', AllDiagnosticCenters.as_view(), name='all-centers'),
+    path('all-centers/', search_paginator, name='all-centers'),
     path('admin-login/', admin_login, name='admin-login'),
     path('admin-logout/', admin_logout, name='admin-logout'),
     path('admin-dashboard/', AdminDashboard.as_view(), name='admin-dashboard'),
