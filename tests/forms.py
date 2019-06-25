619aac241c2cb9b8603d39e944912bc3660c8685
siyam04 <galib.abdullah04@gmail.com>
2019-06-25 23:29:57 +0600

    'db.sqlite3' added to 'master'. do not ignore 'DB' from 'master'. 'form-control' added to all forms...

--- tests/forms.py
+++ tests/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import TextInput, Select, Textarea, NumberInput, SelectDateWidget, TimeInput
+from django.forms import TextInput, Select, SelectDateWidget, TimeInput
 
 from .models import TestOrder, Test
 
@@ -10,9 +10,14 @@ class TestOrderForm(forms.ModelForm):
         fields = ['client_info', 'contact_no', 'email', 'address', 'test_info', 'payment_option', 'date', 'time']
 
         widgets = {
+            'client_info': Select(attrs={'class': 'form-control'}),
+            'contact_no': TextInput(attrs={'class': 'form-control'}),
+            'email': TextInput(attrs={'class': 'form-control'}),
+            'address': TextInput(attrs={'class': 'form-control'}),
+            'test_info': Select(attrs={'class': 'form-control'}),
             'payment_option': Select(attrs={'class': 'form-control'}),
             'date': SelectDateWidget(attrs={'class': 'form-control'}),
-            'time': TimeInput(format='%H:%M', attrs={'type': 'time'}),
+            'time': TimeInput(format='%H:%M', attrs={'type': 'time', 'class': 'form-control'}),
         }
 
 
@@ -22,6 +27,11 @@ class TestAddForm(forms.ModelForm):
         fields = ['test_name', 'image', 'category', 'center', 'discount', 'price', 'active_status']
 
         widgets = {
+            'test_name': TextInput(attrs={'class': 'form-control'}),
+            'category': Select(attrs={'class': 'form-control'}),
+            'center': Select(attrs={'class': 'form-control'}),
+            'discount': TextInput(attrs={'class': 'form-control'}),
+            'price': TextInput(attrs={'class': 'form-control'}),
             'active_status': Select(attrs={'class': 'form-control'}),
         }
 
