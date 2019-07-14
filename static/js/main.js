ffcacd85931301f9fc87d215cbb0fc5d72e162e0
Joy <md.sohanur.rahman.joy@gmail.co>
2019-07-14 21:08:56 +0600

    working on add item

--- static/js/main.js
+++ static/js/main.js
@@ -12,4 +12,21 @@ $(document).ready(function () {
             right: "-350px"
         });
     });
+
+    ///////////////////////////
+    const itemPrice = $("#itemPrice");
+    const cartBtn = $("#cart-btn");
+
+    $('#cart-btn').click(function () {
+
+        const slug = $(this).attr('data-slug');
+        let price = $('#price').text();
+
+        for (let i = 0; i < cartBtn.length; i++) {
+            const cbtn = cartBtn[i];
+            console.log('clicked')
+        }
+
+        // console.log(price);
+    });
 });
