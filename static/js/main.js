7ed580fd440516505cb0fc4cebfa03016fb3806f
mahbub <mdmahbubur81@gmail.com>
2019-07-14 18:18:58 +0600

    working on add to cart

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,37 @@
-$(document).ready(function () {
-    $(".slide-left").click(function () {
-        $(".add-to-cart-box").animate({
-            // width: "350px"
-
-            right: "0"
-        });
-    });
-    $(".slide-right").click(function () {
-        $(".add-to-cart-box").animate({
-            // width: "0"
-            right: "-350px"
-        });
-    });
-});
+// $(document).ready(function () {
+//     $(".slide-left").click(function () {
+//         $(".add-to-cart-box").animate({
+//             right: "0"
+//         });
+//     });
+//     $(".slide-right").click(function () {
+//         $(".add-to-cart-box").animate({
+//             right: "-350px"
+//         });
+//     });
+// });
 
 
 const cartBtn = document.getElementsByClassName('cart-btn');
 const price = document.getElementsByClassName('price');
+const slug = document.getElementsByTagName("button")[0].getAttribute('data-price');
 
-for (var i = 0; i < cartBtn.length; i++) {
-    var addItem = cartBtn[i];
+// for (var i = 0; i < cartBtn.length; i++) {
+//     var addItem = cartBtn[i];
 
-    addItem.addEventListener('click', function () {
-        console.log(cartBtn)
+//     addItem.addEventListener('click', function () {
+//         console.log(slug)
+//     });
+// }
+
+$(document).ready(function () {
+    $(".cartBtn").click(function () {
+        for (var i = 0; i < cartBtn.length; i++) {
+            var addItem = cartBtn[i];
+
+            addItem.addEventListener('click', function () {
+                console.log(slug)
+            });
+        }
     });
-}
+});
