0132790d89e3116e4e1c867ac6a07e8b462b5bc0
siyam04 <galib.abdullah04@gmail.com>
2019-08-02 01:33:50 +0600

    report_details info sync updated

--- report_processing/views.py
+++ report_processing/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 
 from custom_users.models import Profile
+from tests.models import Test, TestOrder
 
 from .models import PaymentValidation
 from .forms import PaymentValidationForm
@@ -30,9 +31,18 @@ def all_reports(request):
 def single_report_details(request, id=None):
     report_details = PaymentValidation.objects.get(id=id)
 
+    total_price = int(report_details.approved_order.test_info.price - report_details.approved_order.test_info.discount)
+
+    due_price = int(
+        (report_details.approved_order.test_info.price - report_details.approved_order.test_info.discount) / 2)
+
     template = 'report_processing/single_report_details.html'
 
-    context = {'report_details': report_details}
+    context = {
+        'report_details': report_details,
+        'total_price': total_price,
+        'due_price': due_price,
+    }
 
     return render(request, template, context)
 
